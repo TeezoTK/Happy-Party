ff35b892e902fe57ec8e765013824ddff211549f
TeezoTK <contact@todigitallygrow.com>
2025-11-05 06:04:54 +0000

    Add routes for posts and post details

--- Happ-Party/src/router/config.tsx
+++ Happ-Party/src/router/config.tsx
@@ -1,4 +1,3 @@
-
 import type { RouteObject } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/home/page";
@@ -10,43 +9,25 @@ import FAQs from "../pages/faqs/page";
 import About from "../pages/about/page";
 import Book from "../pages/book/page";
 
+// NEW: import posts list + post detail
+import Posts from "../pages/posts/posts";
+import PostDetail from "../pages/posts/[slug]";
+
 const routes: RouteObject[] = [
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/packages",
-    element: <Packages />,
-  },
-  {
-    path: "/themes",
-    element: <Themes />,
-  },
-  {
-    path: "/gallery",
-    element: <Gallery />,
-  },
-  {
-    path: "/reviews",
-    element: <Reviews />,
-  },
-  {
-    path: "/faqs",
-    element: <FAQs />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/book",
-    element: <Book />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
+  { path: "/", element: <Home /> },
+  { path: "/packages", element: <Packages /> },
+  { path: "/themes", element: <Themes /> },
+  { path: "/gallery", element: <Gallery /> },
+
+  // NEW
+  { path: "/posts", element: <Posts /> },
+  { path: "/posts/:slug", element: <PostDetail /> },
+
+  { path: "/reviews", element: <Reviews /> },
+  { path: "/faqs", element: <FAQs /> },
+  { path: "/about", element: <About /> },
+  { path: "/book", element: <Book /> },
+  { path: "*", element: <NotFound /> },
 ];
 
 export default routes;
